aed4f52207fefabf546aae81bc9db6eb949cce4b
Ezekiel <iamwebwiz@gmail.com>
2021-11-12 05:28:57 +0100

    update relation factory

--- database/factories/RelationFactory.php
+++ database/factories/RelationFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Person;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RelationFactory extends Factory
@@ -9,7 +10,9 @@ class RelationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'person_id' => Person::factory()->create()->id,
+            'relative_id' => Person::factory()->create()->id,
+            'relationship' => $this->faker->randomElement(['sibling', 'father', 'mother', 'spouse']),
         ];
     }
 }
